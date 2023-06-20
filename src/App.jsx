740cd7742d8cf55c5924361caba518f72026bfe7
Francisco Berti da Cruz <fbc991@live.com>
2023-06-20 13:05:10 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import ProjectList from './components/ProjectList';
-// import Header from './components/header';
+import Header from './components/Header';
 import { GlobalStyle } from './styles';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      {/* <Header /> */}
+      <Header />
       <About />
       <ProjectList />
       <Contact />
